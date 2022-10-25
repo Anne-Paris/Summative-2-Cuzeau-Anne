09c8b12f1889e234277b23d708f956d0524e7b31
oem123 <acuzeau@stedwards.edu>
2022-10-25 16:55:51 -0500

    set up

---
+++ src/main/java/com/company/Summative2CuzeauAnne/Summative2CuzeauAnneApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative2CuzeauAnne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative2CuzeauAnneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative2CuzeauAnneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Summative2CuzeauAnne/Summative2CuzeauAnneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative2CuzeauAnne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative2CuzeauAnneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
