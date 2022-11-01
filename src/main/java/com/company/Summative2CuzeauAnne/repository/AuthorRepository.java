8372de62b12696895df809edd86aea1e7a10e3e1
oem123 <acuzeau@stedwards.edu>
2022-11-01 15:16:24 -0500

    video added

--- src/main/java/com/company/Summative2CuzeauAnne/repository/AuthorRepository.java
+++ src/main/java/com/company/Summative2CuzeauAnne/repository/AuthorRepository.java
@@ -1,7 +1,6 @@
 package com.company.Summative2CuzeauAnne.repository;
 
 import com.company.Summative2CuzeauAnne.model.Author;
-import com.company.Summative2CuzeauAnne.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
